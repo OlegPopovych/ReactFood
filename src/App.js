54650fbacfc06f32fd35a12fa18af4840f1a0ebc
Oleg <olezhkapopovych@gmail.com>
2022-07-04 23:38:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,109 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
+
+const Header = () => {
+	return <h2>Hello1</h2>
+};
+
+// const Field = () => {
+// 	const holder = 'type here'
+// 	const styledField = {
+// 		width: "300px"
+// 	};
+// 	return <input
+// 		placeholder={holder}
+// 		type='text'
+// 		style={styledField} />
+// }
+
+class Field extends Component {  // запис через класи, попередньо імпортуємо Component із React бібліотеки
+	render() {
+		const holder = 'type here'
+		const styledField = {
+			width: "300px"
+		};
+		return <input
+			placeholder={holder}
+			type='text'
+			style={styledField} />
+	}
+}
+
+
+
+
+
+function Btn() {
+	const text1 = 'log in'
+	const res = () => {
+		return 'log in'
+	}
+	const logged = false;
+	//return <button>{res()}</button>
+	return <button>{logged ? 'Enter' : text1}</button>
+
+}
+
+class WhoImI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 33,
+			text: "++",
+			position: ""
+		}
+	}
+
+	nextYear = () => {
+		console.log('+++');
+		this.setState(state => ({   //синхронний код, використано колбек, коли результат задежить від попереднього стану,
+			years: state.years + 1   // бо додоє 1 до попереднього якаий має бути
+		}))								// без колбека - код буде асинхронрний	 і може загубитись попередній стан
+	}
+
+	commitInputChanges = (e, color) => {
+		console.log(color);
+		this.setState({
+			position: e.target.value
+		})
+	}
+
+	render() {
+		const { name, surname, link } = this.props;
+		const { position, years } = this.state;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age - {years}, посада - {position}</h1>
+				<a href={link}>Mi profile</a>
+				<form>
+					<span>Введіть посаду</span>
+					<input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+				</form>
+			</div>
+		)
+	}
+}
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+			<Header />
+			<Field />
+			<Btn />
+			<WhoImI name="John" surname="Smidt" link="https://google.com" />
+			<br />
+			<br />
+			<br />
+			<WhoImI name="Nick" surname="Smidt" link="https://google.com" />
+
+
+
+
+			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +116,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*   // lesson 1
+
+//const elem = <h2>Hello world!</h2>;    // React елемент
+//or:
+//const elem = React.createElement('h2', { className: "greatings" }, "Hello world!");  //так ніхто не робить!!!
+
+const text = 'Hello World!';
+const elem = (
+	<div>
+		<h1 className='text'>Текст: {text}</h1>
+		<input type="text" />
+		<lebel htmlFor="123"></lebel>
+		<button tabIndex={0}>click</button>
+	</div>
+);  // class, for - слова зерезервовані, замість них писати className, htmlFor
+
+*/
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	//elem,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
